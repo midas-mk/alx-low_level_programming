f2e9dc6d6c7ed1e2ed27f3663a37fa5d521c0c27
midas-mk <martinkaranja818@gmail.com>
2022-03-10 08:34:29 -0800

    low level progrmming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
-	/* using printf */
+/* using printf */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the size of variuos data types
+ * Return: 0 if exited properly 
+*/ 	
+int main(void)
+{
+	printf("size of a char: %d bytes(s)\n", sizeof(char));
+	printf("size of an int: %d bytes(s)\n", sizeof(int));
+	printf("size of a long int: %d bytes(s)\n", sizeof(long int));
+	printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %d bytes(s)\n", sizeof(float));
+	return (0);
+}	
