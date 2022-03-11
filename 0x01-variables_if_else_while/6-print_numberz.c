7bedf678b491bfa9d5056ae96a8155beffadedaf
midas-mk <martinkaranja818@gmail.com>
2022-03-11 13:43:15 -0800

    low level programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ **
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
