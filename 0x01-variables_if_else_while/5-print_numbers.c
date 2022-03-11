22ccfcaf459b1d3695fa5caa28cda6f416f26c14
midas-mk <martinkaranja818@gmail.com>
2022-03-11 13:30:28 -0800

    low level programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+i#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ **
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
