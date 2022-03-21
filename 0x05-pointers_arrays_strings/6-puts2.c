459683d22342303f4d422df7919844399fffceba
midas-mk <martinkaranja818@gmail.com>
2022-03-21 08:43:50 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,15 +7,15 @@
 
 void puts2(char *str)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		{
+			_putchar(*(str + i));
+		}
+		i += 1;
 	}
 	_putchar('\n');
 }
