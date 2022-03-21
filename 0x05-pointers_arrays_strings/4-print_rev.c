d48616f6985e2684dbfae5879366a684fb18a591
midas-mk <martinkaranja818@gmail.com>
2022-03-21 08:21:28 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,14 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	i--;
+	while (*(s + i) != '\0')
+		i += 1;
+	i -= 1;
 	while (i >= 0)
 	{
-		_putchar(s[i]);
+		_putchar(*(s + i));
 		i--;
 	}
 	_putchar('\n');
